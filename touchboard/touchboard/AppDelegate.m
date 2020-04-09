db45522aebb0d618f64b655e283e166c3730efdd
Exr0n <spotyie@gmail.com>
2020-04-08 19:06:36 -0700

    event listening!

--- touchboard/touchboard/AppDelegate.m
+++ touchboard/touchboard/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import <Cocoa/Cocoa.h>
+#import <ApplicationServices/ApplicationServices.h>
+#include <Carbon/Carbon.h>
 
 @interface AppDelegate ()
 
@@ -15,8 +18,43 @@
 
 @implementation AppDelegate
 
+CGEventRef myCGEventCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon) {
+    NSLog(@"In the callback");
+    //0x0b is the virtual keycode for "b"
+    //0x09 is the virtual keycode for "v"
+    if ((type != kCGEventKeyDown) && (type != kCGEventKeyUp))
+        NSLog(@"event: %@", event);
+    if (CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode) == kVK_ANSI_S) {
+        NSLog(@"event matched");
+    }
+
+    return event;
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
   // Insert code here to initialize your application
+  NSLog(@"Hello?");
+  CFRunLoopSourceRef runLoopSource;
+
+  CFMachPortRef eventTap = CGEventTapCreate(kCGHIDEventTap, kCGHeadInsertEventTap, kCGEventTapOptionDefault, kCGEventMaskForAllEvents, myCGEventCallback, NULL);
+
+  if (!eventTap) {
+      NSLog(@"Couldn't create event tap!");
+      exit(1);
+  }
+
+  runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0);
+
+  CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopCommonModes);
+
+  CGEventTapEnable(eventTap, true);
+
+  CFRunLoopRun();
+
+  CFRelease(eventTap);
+  CFRelease(runLoopSource);
+
+  NSLog(@"goodbye");
 }
 
 
--- touchboard/touchboard/main.m
+++ touchboard/touchboard/main.m
@@ -12,6 +12,5 @@ int main(int argc, const char * argv[]) {
   @autoreleasepool {
       // Setup code that might create autoreleased objects goes here.
   }
-  NSBeep();
   return NSApplicationMain(argc, argv);
 }
